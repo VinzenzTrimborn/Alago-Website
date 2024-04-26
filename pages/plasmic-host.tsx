4e55b4bdf177d94062f3f2bd0d4851faec385a25
vinzenz <vinzenz.trimborn@tum.de>
2024-04-26 13:49:10 +0200

    Included Analytics

--- pages/plasmic-host.tsx
+++ pages/plasmic-host.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { PlasmicCanvasHost } from '@plasmicapp/loader-nextjs';
 import { PLASMIC } from '@/plasmic-init';
-import { Analytics } from "@vercel/analytics/react"
 
 export default function PlasmicHost() {
   return PLASMIC && <PlasmicCanvasHost />;
